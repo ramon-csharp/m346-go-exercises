df8d32e4a579b1cf2d2778c60f80ce77cb7e41e7
ramon-csharp <ramon.zielke@outlook.com>
2025-12-01 14:18:05 +0100

    task 4 2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,12 @@
 package main
-
+import "math"
+import "fmt"
 // TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
 
+func computeHypotenuse(a float64, b float64) float64{
+	return math.Sqrt(math.Pow(a,2) + math.Pow(b,2))
+}
+
 func main() {
-	// TODO: call the function computeHypotenuse
+	fmt.Printf("Hypotenuse: " + "%.2f\n", computeHypotenuse(3,4)) 
 }
